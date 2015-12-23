8a8749d69a93b9d8bb8f8eeb42cb4e1315ee3606
Ralph Tandetzky <ralph.tandetzky@gmail.com>
2015-12-23 19:14:41 +0100

    Made everything compile again.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,3 @@
-#include "vec3d.h"
 #include "main_window.hpp"
 #include "mat.hpp"
 #include "vec.hpp"
@@ -6,20 +5,6 @@
 #include <QApplication>
 
 #include <cassert>
-
-void test()
-{
-    Vec<double,3> v1 = {{{3.0,2.0,1.0}}};
-    Vec<double,3> v2, v3;
-    v2 += v1;
-    v3 = v1 * 2. - v2 / 2.;
-    v3 = {{{3.0,4.0,12.0}}};
-    const auto x = v1*v2;
-    const auto y = sqrNorm(v1);
-    const auto z = norm(v3);
-    assert( x == y );
-    assert( z == x-1 );
-}
 
 
 static void testVec()
@@ -54,7 +39,6 @@ int main(int argc, char *argv[])
 {
     testVec();
     testMat();
-    test();
 
     QApplication a(argc, argv);
     MainWindow w;
--- render3d.pro
+++ render3d.pro
@@ -1,19 +1,11 @@
 QT       += core gui
-#<<<<<<< HEAD
-#
+
 greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
 QMAKE_CXXFLAGS += -std=c++1z -stdlib=libc++
-#=======
-#CONFIG += c++14
-
-#greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
-#>>>>>>> 67e05be32ac0b70e3b93ae0a4805948a6534caa5
 
 TARGET = render3d
 TEMPLATE = app
 
-
-#<<<<<<< HEAD
 SOURCES += \
     main.cpp \
     main_window.cpp
@@ -21,8 +13,7 @@ SOURCES += \
 HEADERS  += \
     main_window.hpp \
     mat.hpp \
-    trafo_mats.hpp
-    vec3d.h
+    trafo_mats.hpp \
     vec.hpp \
 
 FORMS += \
